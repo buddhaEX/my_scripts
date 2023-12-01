ff7191d333da7240bdc66bc254bc4de8ae724483
buddhaEX <allimperfectlove@proton.me>
2023-12-02 00:08:09 +0100

    Added mint_purge script.

---
+++ mint_purge.sh
@@ -0,0 +1,27 @@
+sudo apt purge \
+libreoffice-* \
+transmission-gtk \
+transmission-common \
+hexchat \
+pix \
+redshift \
+blueman \
+bluetooth \
+bluez \
+bluez-* \
+celluloid \
+hypnotix \
+rhythmbox \
+gnome-screenshot \
+gnome-terminal \
+gnome-terminal-data \
+gnome-calendar \
+timeshift \
+thunderbird
+
+sudo rm -rf \
+/usr/lib/python3/dist-packages/redshift_gtk \
+/etc/libreoffice/registry \
+/usr/lib/python3/dist-packages/blueman/ \
+/usr/lib/x86_64-linux-gnu/rhythmbox/ \
+
